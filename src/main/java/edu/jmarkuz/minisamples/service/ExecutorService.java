8fdd019ee29cd8a512d1145bab81659078ba77f3
marian.kuziv <mi.kuziv@gmail.com>
2024-02-09 19:37:44 +0200

    Added 'example.number' properties for examples run

--- src/main/java/edu/jmarkuz/minisamples/service/ExecutorService.java
+++ src/main/java/edu/jmarkuz/minisamples/service/ExecutorService.java
@@ -3,6 +3,7 @@ package edu.jmarkuz.minisamples.service;
 import edu.jmarkuz.minisamples.exception.FakeErrorException;
 import edu.jmarkuz.minisamples.service.example.Example;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ExecutorService {
 
+    @Value("${example.number:0}")
+    private int exampleNumber;
+
+    private final Example example_0;
     private final Example example_1;
     private final Example example_2;
     private final Example example_4;
@@ -18,23 +23,29 @@ public class ExecutorService {
 
     @EventListener(ContextRefreshedEvent.class)
     public void execute() {
-        // case 1:
-        try {
-            this.example_1.execute();
-        } catch (FakeErrorException e) {
-            this.example_1.showResults();
+        switch (exampleNumber) {
+            case 0 -> example_0.execute();
+
+            case 1 -> {
+                try {
+                    this.example_1.execute();
+                } catch (FakeErrorException e) {
+                    this.example_1.showResults();
+                }
+            }
+
+            case 2 -> example_2.execute();
+
+            case 4 -> {
+                example_4.execute();
+                example_4.showResults();
+            }
+
+            case 5 -> {
+                example_5.execute();
+                example_5.showResults();
+            }
         }
-
-        // case 2:
-        example_2.execute();
-
-        // case 4:
-        example_4.execute();
-        example_4.showResults();
-
-        // case 5:
-        example_5.execute();
-        example_5.showResults();
     }
 
 }
