549226b0eaa5e36375d8c666e4d8877eee232eaa
marian.kuziv <mi.kuziv@gmail.com>
2024-02-09 14:09:30 +0200

    Add FakeErrorException on rollback case

---
+++ src/main/java/edu/jmarkuz/minisamples/exception/FakeErrorException.java
@@ -0,0 +1,7 @@
+package edu.jmarkuz.minisamples.exception;
+
+public class FakeErrorException extends RuntimeException {
+    public FakeErrorException(String message) {
+        super(message);
+    }
+}
